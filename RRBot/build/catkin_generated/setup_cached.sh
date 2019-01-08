3715b753d04787ebb625ebeacc1675ae7e3c0834
colin <colin.g.evans96@gmail.com>
2019-01-08 16:20:35 -0500

    Add gazebo experiment on how to move an arm in gazebo

---
+++ RRBot/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/colin/RRBot/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/colin/RRBot/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/colin/RRBot/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/colin/RRBot/src:$ROS_PACKAGE_PATH"
