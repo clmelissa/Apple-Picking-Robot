465da58c0512528c2b162a61a90ce96f50190f82
melissa <christinamelissa8@gmail.com>
2019-02-20 22:45:39 -0500

    Update arduino script for motor

--- Motor/motor/motor.ino
+++ Motor/motor/motor.ino
@@ -3,15 +3,16 @@ private:
  byte dir_pin;
  byte step_pin;
  int step_count;
+ bool enable;
  
 public:
   Motor() {
   step_count = 0;
   dir_pin = 0;
   step_pin = 0;
+  enable = false;
  }
  void init(byte dir_p, byte step_p) {
-  step_count = 0;
   dir_pin = dir_p;
   step_pin = step_p;
   pinMode(dir_pin, OUTPUT);
@@ -21,10 +22,15 @@ public:
   step_count = 0;
  }
  void rotate() {
+  if (!enable) {
+    return;
+  }
   digitalWrite(step_pin,HIGH);
-  delayMicroseconds(3000);
+  delayMicroseconds(5000);
+//  delay(50);
   digitalWrite(step_pin,LOW);
-  delayMicroseconds(3000);
+  delayMicroseconds(5000);
+//  delay(50);
   step_count++;
  }
  bool clockwise() const {
@@ -35,6 +41,12 @@ public:
  }
  int getStepCount() const {
   return step_count;
+ }
+ void enableMotor() {
+    enable = true;
+ }
+ void disableMotor() {
+    enable = false;
  }
 };
 
@@ -54,6 +66,9 @@ long debounce = 200;
 
 int FULL_REV = 200;
 
+// for Serial com
+String inputString = ""; 
+
 void setup() { 
   Serial.begin(9600);
 
@@ -69,8 +84,10 @@ void setup() {
 void loop() {
   reading = digitalRead(button_pin);
   if (reading == HIGH && previous == LOW && millis() - time > debounce) {
-    if (motor_run)
+    if (motor_run) {
       motor_run = false;
+      digitalWrite(led_pin, LOW);
+    }
     else {
       digitalWrite(led_pin, HIGH);
       motor_run = true;
@@ -89,10 +106,46 @@ void loop() {
     motor2.rotate();
     motor3.rotate();
     motor4.rotate();
-    if (motor1.getStepCount() == FULL_REV) {
-      motor_run = false;
-      digitalWrite(led_pin, LOW);
+  }
+}
+
+void checkString () {
+  if (inputString == "e run") // elbow motor
+    motor1.enableMotor();
+  else if (inputString == "s run") // shoulder motor
+    motor2.enableMotor();
+  else if (inputString == "g run") // gripper motor
+    motor3.enableMotor();
+  else if (inputString == "t run") // twister motor
+    motor4.enableMotor();
+  else if (inputString == "e stop") // elbow motor
+    motor1.disableMotor();
+  else if (inputString == "s stop") // shoulder motor
+    motor2.disableMotor();
+  else if (inputString == "g stop") // gripper motor
+    motor3.disableMotor();
+  else if (inputString == "t stop") // twister motor
+    motor4.disableMotor();
+  else{
+    Serial.print("ERROR: Unknown command ");
+    Serial.println(inputString);
+  }
+    
+}
+
+// occur between each loop
+void serialEvent() {
+  char inChar;
+  while (Serial.available() > 0) {
+    // get the new byte:
+    inChar = (char)Serial.read();
+    // end of command
+    if (inChar == '\n') {
+      checkString();
+      inputString = "";
+    } else {
+      // add it to the inputString:
+      inputString += inChar;
     }
   }
-  
 }
