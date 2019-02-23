85dcccea0e1a8aeb117ffe11e20c0f48a95b111b
melissa <christinamelissa8@gmail.com>
2019-02-23 04:17:08 -0500

    Add changes to motor library
    1. Add delay when changing motor direction
    2. Add optional argument speed(/time delay for stepper motor) to rotate
    & Minor fixes+comments

--- Motor/motor/motor.h
+++ Motor/motor/motor.h
@@ -7,11 +7,18 @@
 
 class Motor {
 private:
+  // Arduino pin
   byte dir_pin;
   byte step_pin;
+
+  // if enable is false then motor won't run even if rotate() is called
   bool enable;
+  // indicate the number of steps between the motor position and the initial position
   int position;
+  // indicate motor direction
   bool CW;
+  // limit for the number of steps can the motor move to 1 direction
+  // default is 70
   int limit;
  
 public:
@@ -26,7 +33,7 @@ public:
 
   // init the direction pin and step pin
   // have to call this function before using the class
-  void init(byte dir_p, byte step_p, int lim = 200) {
+  void init(byte dir_p, byte step_p, int lim = 70) {
     dir_pin = dir_p;
     step_pin = step_p;
     pinMode(dir_pin, OUTPUT);
@@ -35,13 +42,23 @@ public:
     limit = lim;
   }
 
+  // return current position
+  int getPosition() const {
+    return position;
+  }
+
+  bool isClockwise() const {
+    return CW;
+  }
+
   // rotate the stepper motor if the motor is enabled
-  void rotate() {
+  // the lower the speed argument the faster it is
+  void rotate(int speed = 50) {
     if (enable && abs(position) < limit) {
       digitalWrite(step_pin,HIGH);
-      delay(50);
+      delay(speed);
       digitalWrite(step_pin,LOW);
-      delay(50);
+      delay(speed);
       if (CW) {
         position++;
       } else {
@@ -50,21 +67,22 @@ public:
     }
   }
 
+  void setCurrentAsZeroPos() {
+    position=0;
+  }
+
   // change motor direction to clockwise
   bool clockwise() {
-    digitalWrite(dir_pin,HIGH);
+    digitalWrite(dir_pin,LOW);
     CW = true;
+    delay(500);
   }
 
   // change motor direction to counter clockwise
   void counterClockwise() {
-    digitalWrite(dir_pin,LOW);
+    digitalWrite(dir_pin,HIGH);
     CW = false;
-  }
-
-  // return current position
-  int getPosition() const {
-    return position;
+    delay(500);
   }
 
   // move back to zero position
@@ -74,8 +92,13 @@ public:
     } else {
       counterClockwise();
     }
+    int speed = 50;
     while (position!=0) {
-      rotate();
+      // when its close to destination slows down the motor
+      if (abs(position)<10) {
+        speed+=5;
+      }
+      rotate(speed);
     }
   }
 
