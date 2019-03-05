3acb62b8001af1d239e611dc9d9a6a85c24fd665
clmelissa <christinamelissa8@gmail.com>
2019-03-05 06:43:06 -0500

    Add latest change for apple detection webcam

--- Experiments/apple_detection_webcam.py
+++ Experiments/apple_detection_webcam.py
@@ -4,14 +4,14 @@ import cv2
 import math
 
 def get_image_mask(hsv_img, lower_thres, upper_thres):
-  # blurring smooths the image and allows for slightly more accurate circle detection, as part of the next step includes edge detection
+  # blurring smooths the image and allows for slightly more accurate circle detection
   mask = cv2.GaussianBlur(hsv_img, (5, 5), 0)
-  # construct a mask for the hsv image, then perform closing to remove 'salt and pepper'
+  # construct a mask for the hsv image, then perform closing
   mask = cv2.inRange(mask, lower_thres, upper_thres)
   # mask = cv2.dilate(mask, None, iterations=2)
-  opening_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4,4))
-  mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, opening_kernel)
-  closing_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10,10))
+  # opening_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4,4))
+  # mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, opening_kernel)
+  closing_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20,20))
   mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, closing_kernel)
 
   # mask = cv2.erode(mask, None, iterations=2)
@@ -22,8 +22,8 @@ def main():
   # get webcam feed
   camera = cv2.VideoCapture(0)
   # define the lower and upper boundaries of the HSV color space
-  hsv_lower = (5, 92, 0)
-  hsv_upper = (180, 255, 150)
+  hsv_lower = (0, 45, 45)
+  hsv_upper = (18, 225, 150)
 
   while True:
     # Load an image in RGV 
