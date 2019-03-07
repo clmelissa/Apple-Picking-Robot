Fix calculation for vertical movements
Send serial commands to arduino to move up and down by certain number
of steps. 500 steps = 1cm +- 0.1
Add offset to camera as it is not centered
For now the code exit after 1 movement

Experiments/apple_distance_webcam.py
@@ -2,6 +2,8 @@ import numpy as np
 import argparse
 import cv2
 import math
+import serial
+import time
 
 def get_image_mask(hsv_img, lower_thres, upper_thres):
   # blurring smooths the image and allows for slightly more accurate circle detection
@@ -11,7 +13,7 @@ def get_image_mask(hsv_img, lower_thres, upper_thres):
   # mask = cv2.dilate(mask, None, iterations=2)
   # opening_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4,4))
   # mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, opening_kernel)
-  closing_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20,20))
+  closing_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (35,35))
   mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, closing_kernel)
 
   # mask = cv2.erode(mask, None, iterations=2)
@@ -22,14 +24,17 @@ def main():
   # get webcam feed
   camera = cv2.VideoCapture(1)
   # define the lower and upper boundaries of the HSV color space
-  hsv_lower = (0, 75, 10)
-  hsv_upper = (5, 255, 255)
+  hsv_lower = (0, 75, 75)
+  hsv_upper = (15, 255, 240)
 
   # camera focal length calculated by experiment
-  f = 744
+  f = 748
   # finding distance by : Distance = (Apple Diameter x Camera Focal Length) / Pixel Diameter
-  apple_diameter = 5 #in cm
+  apple_diameter = 6.6 #in cm
   apple_const = apple_diameter*f
+
+  # camera offset from arm center in cm
+  camera_offset = 6
 
   while True:
     # Load an image in RGV 
@@ -55,7 +60,7 @@ def main():
       if perimeter>0:
         circularity = 4*math.pi*area/perimeter/perimeter
 
-        if radius > 30 and circularity > 0.7:
+        if radius > 30 and circularity > 0.55:
           # draw the circle on the frame,
           cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
           pixel_diameter = radius*2
@@ -63,17 +68,29 @@ def main():
           x_center = np.shape(frame)[1]/2
           y_center = np.shape(frame)[0]/2
           center = True
-          if x < (x_center - 35):
-            cv2.putText(frame, "Move left" ,(10,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2,cv2.LINE_AA)
+          # if x < (x_center - 35):
+          #   cv2.putText(frame, "Move left" ,(10,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2,cv2.LINE_AA)
+          #   center = False
+          # elif x > (x_center + 35):
+          #   cv2.putText(frame, "Move right" ,(10,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2,cv2.LINE_AA)
+          #   center = False
+
+          pixel_ratio = (pixel_diameter-10)/apple_diameter
+          mov = (abs(y - y_center)/pixel_ratio )
+          
+          pixel_offset = pixel_ratio * camera_offset
+          y_check = y-pixel_offset
+          if y_check < (y_center - 35):
+            mov += camera_offset
+            steps = int(mov*500) - 100
+            cv2.putText(frame, "Up  " + str(mov)+ "cm",(300,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2,cv2.LINE_AA)
+            cv2.putText(frame, "Up  " + str(steps) + "steps",(300,300), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2,cv2.LINE_AA)
             center = False
-          elif x > (x_center + 35):
-            cv2.putText(frame, "Move right" ,(10,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2,cv2.LINE_AA)
-            center = False
-          if y < (y_center - 35):
-            cv2.putText(frame, "Move up" ,(400,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2,cv2.LINE_AA)
-            center = False
-          elif y > (y_center + 35):
-            cv2.putText(frame, "Move down" ,(400,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2,cv2.LINE_AA)
+          elif y_check > (y_center + 35):
+            mov -= camera_offset
+            steps = int(mov*500) - 100
+            cv2.putText(frame, "Down  " + str(mov) + "cm",(300,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2,cv2.LINE_AA)
+            cv2.putText(frame, "Down  " + str(steps) + "steps",(300,300), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2,cv2.LINE_AA)
             center = False
           if center:
             cv2.putText(frame, "CENTER" ,(10,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2,cv2.LINE_AA)